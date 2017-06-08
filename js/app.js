98c39c5296d8b17d41c7c7cfdbc7acc9ec083990
= <=>
2017-06-08 17:39:30 -0400

    Prevent NaN from appearing in calc table

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ angular.module('PathOfDamage', [])
   };
 
   $scope.updateDamageValues = function (skipSerialization) {
-    $scope.hits = $scope.hits.map(function (hit) { return calcDamage(hit.hit) });
+    $scope.hits = $scope.hits.map(function (hit) { return hit.hit ? calcDamage(hit.hit) : null });
     if (!skipSerialization) {
       serializeData();
     }
